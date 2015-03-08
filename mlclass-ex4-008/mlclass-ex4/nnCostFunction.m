0e45bb829cdfbb61b68d429386a2fc1eda670548
Valentin LEHUGER <vlehuger@e3r5p1.42.fr>
2015-03-08 14:03:19 +0100

    added backprop vectorized

--- mlclass-ex4-008/mlclass-ex4/nnCostFunction.m
+++ mlclass-ex4-008/mlclass-ex4/nnCostFunction.m
@@ -63,26 +63,24 @@ Theta2_grad = zeros(size(Theta2));
   %
 
 
-y_mat = bsxfun(@eq, y, [1:max(y)]);
+  Y = bsxfun(@eq, y, 1:num_labels);
 
-a1 = [ones(m, 1) X];
-
-z2 = a1 * Theta1';
-
-a2 = sigmoid ( z2);
+a1 = [ones(m, 1), X];
+z2 = a1 * Theta1';                                       %'
+a2 = sigmoid(z2);
 a2 = [ones(m, 1) a2];
+z3 = a2 * Theta2';                                        %'
+hypothesis = sigmoid(z3);
 
-z3 = a2 * Theta2';
+cost = (-Y .* log(hypothesis)) - ( (1 - Y) .* log(1 - hypothesis));
+J = (1/m) * sum( cost(:) );
 
-a3 = sigmoid (z3);
+Theta1_filtered_squared = Theta1(:, 2:end) .^2;
+Theta2_filtered_squared = Theta2(:, 2:end) .^2;
 
-J = (1/m) * sum(sum( -y_mat .* log (a3) - (1 - y_mat) .* log(1 - a3) ));
+J = J + ((lambda / (2 * m)) * (sum(Theta1_filtered_squared(:)) + sum(Theta2_filtered_squared(:))));
 
-reg_terms = (lambda / (2*m)) * ( sum(sum(Theta1(:, 2:end).^2)) + sum(sum(Theta2(:, 2:end).^2)) );
-
-J = J + reg_terms;
-
-d3 = a3 - y_mat;
+d3 = hypothesis - Y;
 
 d2 = ( d3 * Theta2(:, 2:end) ) .* sigmoidGradient(z2);
 
@@ -94,6 +92,8 @@ Theta2_grad = (1/m) * D2;
 
 Theta1_grad(:,2:end) = Theta1_grad(:,2:end) + (lambda / m) * Theta1(:, 2:end);
 Theta2_grad(:,2:end) = Theta2_grad(:,2:end) + (lambda / m) * Theta2(:, 2:end);
+
+
 
 % -------------------------------------------------------------
 
